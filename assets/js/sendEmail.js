2b8dc7d568df2afc0d2290a86b8993f1bf18459a
jonathanw82 <jonwhewaycode@outlook.com>
2020-02-27 21:45:16 +0000

    email sends but not populated

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,14 +1,7 @@
-console.log("hello");
 
-/*
-    <script type="text/javascript">
-        window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                // generate the contact number value
-                this.contact_number.value = Math.random() * 100000 | 0;
-                emailjs.sendForm('gmail', 'jon', this);
-            });
-        }
-    </script>
-    */
+
+
+
+
+  
+    
